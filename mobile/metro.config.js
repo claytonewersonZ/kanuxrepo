5bccadf3e5ddd5bbe08ec7d464e6dc2bc310d181
jefferson diogo <diogojefferson206@gmail.com>
2026-06-15 21:15:12 -0300

    so testando uma coisa

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,7 +1,16 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.resolver = {
+  ...config.resolver,
+  resolveRequest: (context, moduleName, platform) => {
+    if (moduleName.startsWith('@opentelemetry/')) {
+      return { type: 'empty' };
+    }
+    return context.resolveRequest(context, moduleName, platform);
+  },
+};
 
 module.exports = config;
